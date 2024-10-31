ac42a0eef9afbecc9bd821916876755c09d469fe
wirehest <1033169+wirehest@users.noreply.github.com>
2024-11-01 00:30:11 +0900

    Refactor Pig Latin (JS) to use switch statements

--- javascript/pig-latin/pig-latin.js
+++ javascript/pig-latin/pig-latin.js
@@ -1,33 +1,28 @@
-/** Translates English to Pig Latin. */
-export const translate = (phrase) => {
-  const consonants = ['a', 'e', 'i', 'o', 'u'];
-  let words = phrase.toLowerCase().split(' ');
-
-  let piglatinified = words.map((x) => {
-    if (consonants.includes(x.at(0))) {
-      return x + 'ay';
-    }
-
-    if (x.startsWith('squ') || x.startsWith('thr') || x.startsWith('sch')) {
-      return x.slice(3) + x.slice(0, 3) + 'ay';
-    }
-
-    if (x.startsWith('qa')) {
-      return x.slice(1) + 'qay';
-    }
-
-    if (x.startsWith('qu') || x.startsWith('ch') || x.startsWith('th')) {
-      return x.slice(2) + x.slice(0, 2) + 'ay';
-    }
-
-    if (x.startsWith('yt') || x.startsWith('xr')) {
-      return x + 'ay';
-    }
-    if (x.startsWith('rhy')) {
-      return x.slice(2) + 'rhay';
-    }
-
-    return x.slice(1) + x.at(0) + 'ay';
-  });
-  return piglatinified.join(' ');
-};
+export function translate(phrase) {
+  return phrase
+    .toLowerCase()
+    .split(' ')
+    .map((word) => {
+      switch (true) {
+        case word.startsWith('squ'):
+        case word.startsWith('thr'):
+        case word.startsWith('sch'):
+          return word.slice(3) + word.slice(0, 3) + 'ay';
+        case word.startsWith('qa'):
+          return word.slice(1) + 'qay';
+        case word.startsWith('qu'):
+        case word.startsWith('ch'):
+        case word.startsWith('th'):
+          return word.slice(2) + word.slice(0, 2) + 'ay';
+        case word.startsWith('yt'):
+        case word.startsWith('xr'):
+        case 'aeiou'.includes(word[0]):
+          return word + 'ay';
+        case word.startsWith('rhy'):
+          return word.slice(2) + 'rhay';
+        default:
+          return word.slice(1) + word[0] + 'ay';
+      }
+    })
+    .join(' ');
+}
