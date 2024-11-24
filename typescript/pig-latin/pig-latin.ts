4b0f4bac5516fe46ef5cbba3ab2d321c2625feb9
wirehest <1033169+wirehest@users.noreply.github.com>
2024-11-24 21:29:59 +0900

    Refactor TypeScript solution: Pig Latin
    - Replace method and string-based matching to regular expressions for
      conciseness.

--- typescript/pig-latin/pig-latin.ts
+++ typescript/pig-latin/pig-latin.ts
@@ -1,28 +1,34 @@
 export function translate(phrase: string): string {
+  const AY = 'ay';
   return phrase
-    .toLowerCase()
     .split(' ')
     .map((word) => {
-      switch (true) {
-        case word.startsWith('squ'):
-        case word.startsWith('thr'):
-        case word.startsWith('sch'):
-          return word.slice(3) + word.slice(0, 3) + 'ay';
-        case word.startsWith('qa'):
-          return word.slice(1) + 'qay';
-        case word.startsWith('qu'):
-        case word.startsWith('ch'):
-        case word.startsWith('th'):
-          return word.slice(2) + word.slice(0, 2) + 'ay';
-        case word.startsWith('yt'):
-        case word.startsWith('xr'):
-        case 'aeiou'.includes(word[0]):
-          return word + 'ay';
-        case word.startsWith('rhy'):
-          return word.slice(2) + 'rhay';
-        default:
-          return word.slice(1) + word[0] + 'ay';
-      }
+      let match = getMatchingBeginning(word);
+      return match.length > 0
+        ? `${word.slice(match.length)}${match}${AY}`
+        : `${word}${AY}`;
     })
     .join(' ');
+}
+
+function getMatchingBeginning(word: string): string {
+  const REGEX_PATTERNS = {
+    STARTS_WITH_VOWEL_SOUND: /^[aeiou]|xr|xt/,
+    STARTS_WITH_CONSONANTS: /^[^aeiou]+/,
+    CONSONANTS_WITH_QU: /^[^aeiou]*qu/,
+    CONSONANTS_WITH_Y: /^[^aeiou]+(?=y)/,
+  } as const;
+
+  switch (true) {
+    case REGEX_PATTERNS.CONSONANTS_WITH_Y.test(word):
+      return word.match(REGEX_PATTERNS.CONSONANTS_WITH_Y)![0];
+    case REGEX_PATTERNS.CONSONANTS_WITH_QU.test(word):
+      return word.match(REGEX_PATTERNS.CONSONANTS_WITH_QU)![0];
+    case REGEX_PATTERNS.STARTS_WITH_CONSONANTS.test(word):
+      return word.match(REGEX_PATTERNS.STARTS_WITH_CONSONANTS)![0];
+    case REGEX_PATTERNS.STARTS_WITH_VOWEL_SOUND.test(word):
+      return '';
+    default:
+      throw new Error('no pattern matched');
+  }
 }
