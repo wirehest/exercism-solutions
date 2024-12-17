b4ee8fa852bc8bf31eed0a210a4692c88ed7be11
wirehest <1033169+wirehest@users.noreply.github.com>
2024-12-17 19:01:14 +0900

    Refactor Resistor Color Trio (TypeScript)
    - Changed to regex to find trailing zeros. Now we don't need a one-of to
      catch the '0 ohms' edge case.
    - Added explicit type for the PREFIX object keys.

--- typescript/resistor-color-trio/resistor-color-trio.ts
+++ typescript/resistor-color-trio/resistor-color-trio.ts
@@ -1,4 +1,6 @@
 type Resistors = (typeof COLORS)[number];
+type PrefixKey = keyof typeof PREFIXES;
+
 const COLORS = [
   'black',
   'brown',
@@ -11,21 +13,21 @@ const COLORS = [
   'grey',
   'white',
 ] as const;
-const METRIC = { 0: '', 2: 'mega', 3: 'kilo', 6: 'mega', 9: 'giga' } as const;
+const PREFIXES = { 3: 'kilo', 6: 'mega', 9: 'giga' } as const;
 
 export function decodedResistorValue(bands: Resistors[]): string {
-  let first = String(COLORS.findIndex((color) => color === bands[0]));
-  let second = String(COLORS.findIndex((color) => color === bands[1]));
-  let zeroes = '0'.repeat(COLORS.findIndex((color) => color === bands[2]));
-  let ohms = +(first + second + zeroes);
-  let result = '';
+  let trailingZerosPattern = /0{9}|0{6}|0{3}/;
+  let fullString = bands
+    .slice(0, 2)
+    .reduce((acc, color) => acc + COLORS.indexOf(color), '');
+  fullString += '0'.repeat(COLORS.indexOf(bands[2]));
 
-  if (!ohms) return '0 ohms';
-  for (let [exp, prefix] of Object.entries(METRIC).reverse()) {
-    if (ohms % 10 ** +exp === 0) {
-      result = `${ohms / 10 ** +exp} ${prefix}ohms`;
-    }
-  }
+  let trailingZeroCount = trailingZerosPattern.test(fullString)
+    ? (fullString.match(trailingZerosPattern)![0].length as PrefixKey)
+    : (0 as PrefixKey);
 
-  return result;
+  let significantDigits = +fullString.replace(trailingZerosPattern, '');
+  let prefix = PREFIXES[trailingZeroCount];
+
+  return `${significantDigits} ${prefix}ohms`;
 }
