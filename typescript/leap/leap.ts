d7cb4fa45a473f347e126760fddf64fdc53bfdde
wirehest <1033169+wirehest@users.noreply.github.com>
2024-12-31 17:38:55 +0900

    Refactor Leap (TypeScript)
    - Remove explicit true/false returns (unnecessary) and simplify logic.

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -1,12 +1,3 @@
 export function isLeap(year: number): boolean {
-  if (!(year % 4)) {
-    if (!(year % 100 || year % 400)) {
-      return true;
-    } else if (!(year % 100) && year % 400) {
-      return false;
-    } else {
-      return true;
-    }
-  }
-  return false;
+  return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
 }
