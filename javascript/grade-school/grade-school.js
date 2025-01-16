f0e19fd81ccbcdf3b85fe7b0ab866e20ede644a4
wirehest <1033169+wirehest@users.noreply.github.com>
2025-01-17 02:56:39 +0900

    Refactor Grade School (JavaScript)
    - Remove stringification-based solution to deduplication and rely on
      inherent Object qualities (i.e., unique keys) instead.
    - Change structuredClone to spread for grade method.

--- javascript/grade-school/grade-school.js
+++ javascript/grade-school/grade-school.js
@@ -1,32 +1,28 @@
-/** Representation of a school roster with grades and names. */
 export class GradeSchool {
-  constructor() {
-    this.fullRoster = {};
-  }
+  #roster = {};
+  #students = {};
 
   roster() {
-    return structuredClone(this.fullRoster);
+    let classYears = Array.from(new Set(Object.values(this.#students)));
+
+    for (let year of classYears) {
+      let classRoster = Object.entries(this.#students)
+        .filter(([student, classYear]) => {
+          return classYear === year;
+        })
+        .map(([student, _]) => student)
+        .sort();
+      this.#roster[year] = classRoster;
+    }
+
+    return structuredClone(this.#roster);
   }
 
-  add(name, grade) {
-    if (JSON.stringify(this.fullRoster).includes(name)) {
-      this.fullRoster = JSON.parse(
-        JSON.stringify(this.fullRoster).replace(
-          new RegExp(`"${name}"[,]?`, 'giu'),
-          ''
-        )
-      );
-    }
-    if (this.fullRoster[grade]) {
-      this.fullRoster[grade].push(name);
-    } else {
-      this.fullRoster[grade] = [name];
-    }
-    Object.keys(this.fullRoster).map((x) => this.fullRoster[x].sort());
+  add(student, classYear) {
+    this.#students[student] = classYear;
   }
 
-  grade(grade) {
-    if (!this.fullRoster[grade]) this.fullRoster[grade] = [];
-    return structuredClone(this.fullRoster[grade]);
+  grade(classYear) {
+    return [...(this.roster()[classYear] ?? [])];
   }
 }
